f0520de80a230fe580febdc99474c4e61cf831d6
natswatch <nathaliec@me.com>
2020-12-22 18:36:42 -0800

    saveBook and removeBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -54,12 +54,14 @@ const resolvers = {
     },
     removeBook: async(parent, args, context) => {
       if(context.user) {
-        const updatedUser = await User.findByIdAndUpdaate(
-          { _id: context.user.id },
-          { $pull: { savedBooks: {bookId: args.bookId} }},
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: args }},
           { new: true }
         ).populate('savedBooks');
+
         return updatedUser;
+
       }
       throw new AuthenticationError('You need to be logged in!')
     }
